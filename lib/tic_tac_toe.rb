dd7f0ba746b0edfc109178d21442c7cddb6fc48d
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-24 01:12:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,10 +84,10 @@ class TicTacToe
  
   def won?
     WIN_COMBINATIONS.each do |win|
-      if @board[win[0]] == "X" && @board[win[1]] == "1" && @board[win[2]] =="X"
+      if @board[win[0]] == "X" && @board[win[1]] == "X" && @board[win[2]] =="X"
         return win
 
-      elsif @board[win[0]] == "2" && @board[win[1]] == "2" && @board[win[2]] =="2"
+      elsif @board[win[0]] == "O" && @board[win[1]] == "O" && @board[win[2]] =="O"
         return win
       end
     end
